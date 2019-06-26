7658e6b3dcff49ccbde841ed7f587ceb59a98f23
Regis Queffelec <rqueffelec@dxc.com>
2019-06-26 14:29:10 +0930

    Added Courses to the tutorial to introduce props and state

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Administration</h1>
+      <div>React, Flux and React Router for ultra-responsive web apps.</div>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
